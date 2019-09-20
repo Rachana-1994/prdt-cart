5484a71d32fc2e835c1829a647601e1ef0129662
lavanya-sudha <lavanyasudha.g@gmail.com>
2019-09-20 20:14:46 +0530

    prdt-cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  //styleUrls: ['./app.component.css']
 })
-/*export class AppComponent {
-  title = 'ngmat';
-}*/
+
 export class AppComponent implements OnInit {
 
   ngOnInit() {
