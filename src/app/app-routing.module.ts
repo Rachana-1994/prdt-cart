f201f96f7c3b552ea4db675154148a4f8db4729a
lavanya-sudha <lavanyasudha.g@gmail.com>
2019-09-25 20:56:44 +0530

    prdt and cart comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { IndexComponent } from './index/index.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-	{ path: '', component: ProductComponent },
+	//{ path: '', component: ProductComponent },
 	{ path: 'products', component: ProductComponent },
 	{ path: 'cart', component: CartComponent },
 	{ path: '**', redirectTo: '' }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,23 @@ import { IndexComponent } from './index/index.component';
 import { CartComponent } from './cart/cart.component';
 import { routing } from './app-routing.module';
 import { ProductService } from './product.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MyTableComponent } from './my-table/my-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -13,11 +30,28 @@ import { ProductService } from './product.service';
     AppComponent,
     ProductComponent,
     IndexComponent,
-    CartComponent
+    CartComponent,
+    MyNavComponent,
+    MyDashboardComponent,
+    MyTableComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTabsModule
    
   ],
   providers: [ProductService],
